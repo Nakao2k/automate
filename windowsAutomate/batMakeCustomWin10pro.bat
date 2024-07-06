970ede07f1228c6e54dc447068efaafd6217ae91
Nakao2k <abcdown55@outlook.jp>
2024-07-07 01:29:51 +0900

    New file

---
+++ windowsAutomate/batMakeCustomWin10pro.bat
@@ -0,0 +1,22 @@
+:: 作業ディレクトリ
+cd OS_Windows10_Pro_22H2
+
+:: 作成する ISO ファイル名
+set ISOFile=..\OS_CUSTOM_Windows10_Pro_22H2.iso
+
+:: ISO にするフォルダ
+set ISOBase=.
+
+:: アーキテクチャ
+set Architecture=amd64
+
+:: 起動ファイル
+set ISO_etfsboot=boot\etfsboot.com
+set ISO_efisys=efi/microsoft/boot/efisys.bin
+
+:: ISO 作成
+set ISOCommand="C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\%Architecture%\Oscdimg\oscdimg.exe" -m -o -u2 -udfver102 -bootdata:2#p0,e,b%ISO_etfsboot%#pEF,e,b%ISO_efisys% . %ISOFile%
+
+%ISOCommand%
+
+pause
