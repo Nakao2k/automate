975bc82b407751d016097d1600e45fff70aaa3d5
Nakao2k <abcdown55@outlook.jp>
2024-08-04 03:38:13 +0900

    Modified

--- windowsAutomate/batMakeCustomWin2022std_jp.bat
+++ windowsAutomate/batMakeCustomWin2022std_jp.bat
@@ -7,15 +7,13 @@ set ISOFile=..\OS_CUSTOM_WindowsServer_2022_trial_std.iso
 :: ISO にするフォルダ
 set ISOBase=.
 
-:: アーキテクチャ
-set Architecture=amd64
-
 :: 起動ファイル
+set ISO_exe="%ProgramFiles(x86)%\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\%PROCESSOR_ARCHITECTURE%\Oscdimg\oscdimg.exe"
 set ISO_etfsboot=boot\etfsboot.com
 set ISO_efisys=efi/microsoft/boot/efisys.bin
 
 :: ISO 作成
-set ISOCommand="C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\%Architecture%\Oscdimg\oscdimg.exe" -lWin2022std -m -o -u2 -udfver102 -bootdata:2#p0,e,b%ISO_etfsboot%#pEF,e,b%ISO_efisys% . %ISOFile%
+set ISOCommand=%ISO_exe% -lWin2022std -m -o -u2 -udfver102 -bootdata:2#p0,e,b%ISO_etfsboot%#pEF,e,b%ISO_efisys% . %ISOFile%
 
 %ISOCommand%
 
